7cbb7b9c7a0a1c9a8806d397dd775b9f5d1064b4
Luca Wahlen <mail@lucawahlen.de>
2025-12-16 16:33:13 +0100

    Refactor database initialization paths and add user seed data

---
+++ db/init/local-dev/01_users.sql
@@ -0,0 +1,24 @@
+INSERT INTO gym_manager.users (
+    user_email,
+    password_hash,
+    user_firstname,
+    user_lastname,
+    user_middlename,
+    user_birthdate,
+    user_address,
+    user_phone,
+    payment_type,
+    payment_info
+) VALUES (
+             'admin@admin.com',
+             '$2a$10$3W1fOUdvSq7DxxuqP58T1OR0AViu42IxdZtBgxNeWUXlNx0j3HfjK', -- admin
+             'Demo',
+             'User',
+             NULL,
+             '1990-01-01',
+             '123 Demo Street, Atlas City',
+             '+1234567890',
+             'credit_card',
+             '4111 1111 1111 1111'
+         );
+
