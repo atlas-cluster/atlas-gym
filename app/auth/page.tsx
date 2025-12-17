4b6d7681d88875aae580d7dd3a61cfda9c2da03a
Luca Wahlen <mail@lucawahlen.de>
2025-12-18 00:07:47 +0100

    Refactor layout in login page: add container for improved structure and styling

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -5,15 +5,15 @@ import { LoginForm } from '@/components/login-form'
 import { RegisterForm } from '@/components/register-form'
 
 export default async function Page({
-  searchParams,
-}: {
+                                     searchParams,
+                                   }: {
   searchParams: Promise<{ redirect?: string }>
 }) {
   const params = await searchParams
   const redirectTo = params.redirect || '/'
 
   return (
-    <div className="flex min-h-screen w-full justify-center px-2 pt-20 pb-2">
+    <div className="flex min-h-screen w-full justify-center p-2 items-center">
       <div className="flex w-full max-w-sm flex-col items-center gap-8">
         <Image
           src={'/atlas_logo_rounded_m.png'}
@@ -21,20 +21,22 @@ export default async function Page({
           height={100}
           alt={'Logo'}
         />
-        <Card className={'w-full pt-0'}>
-          <Tabs defaultValue="login">
-            <TabsList className={'mb-8 flex w-full'}>
-              <TabsTrigger value="login">Login</TabsTrigger>
-              <TabsTrigger value="register">Register</TabsTrigger>
-            </TabsList>
-            <TabsContent value="login">
-              <LoginForm redirectTo={redirectTo} />
-            </TabsContent>
-            <TabsContent value="register">
-              <RegisterForm />
-            </TabsContent>
-          </Tabs>
-        </Card>
+        <div className={"min-h-[500px] w-full"}>
+          <Card className={'w-full pt-0'}>
+            <Tabs defaultValue="login">
+              <TabsList className={'mb-8 flex w-full'}>
+                <TabsTrigger value="login">Login</TabsTrigger>
+                <TabsTrigger value="register">Register</TabsTrigger>
+              </TabsList>
+              <TabsContent value="login">
+                <LoginForm redirectTo={redirectTo} />
+              </TabsContent>
+              <TabsContent value="register">
+                <RegisterForm />
+              </TabsContent>
+            </Tabs>
+          </Card>
+        </div>
       </div>
     </div>
   )
